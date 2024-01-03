401862e6dc8c0c57fff02520d669911cfefaba75
BehrozKarim <behrozbuzdar@gmail.com>
2024-01-03 19:34:58 +0500

    initializing the todo-app

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express } from "express";
+import bodyParser from 'body-parser'
+
+const app: Express = express()
+app.use(bodyParser.json({limit: "100mb"}))
+app.use(bodyParser.urlencoded({limit:"50mb", extended: true}))
+
+app.listen(5000, () => {
+    console.log("App running on 5000 port")
+})
+
+// return hello world on get request
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
