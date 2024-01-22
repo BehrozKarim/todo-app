3fe24725dbb94d737edb4319bbaa0db46425bf91
BehrozKarim <behrozbuzdar@gmail.com>
2024-01-22 18:20:30 +0500

    feat: logg each requst coming to the server

--- src/http/bootstrap.ts
+++ src/http/bootstrap.ts
@@ -1,11 +1,13 @@
 import router from "./routes/routes";
 import express, {Express} from "express";
 import bodyParser from 'body-parser'
+import logger from "./middlewares/logger-middleware";
 
 const app: Express = express();
 
 app.use(bodyParser.json({limit: "100mb"}))
 app.use(bodyParser.urlencoded({limit:"50mb", extended: true}))
+app.use(logger);
 
 app.use("/", router);
 
